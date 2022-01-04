fc51f3239ca3576132ae837996797931aaf84daa
Jamie <drpcom@yahoo.com>
2022-01-04 16:13:44 -0600

    Might switch sidebar to flex.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './Sidebar.css';
 
 const Sidebar = () => {
     return (
-        <div>
-            <h1>here are the tags</h1>
+        <div className="sidebar-container">
+            <div className="sidebar-title">
+                <h2>Tags</h2>
+            </div>
+            <ul className="sidebar-content">
+                <li>falling</li>
+                <li>being chased</li>
+                <li>dying</li>
+                <li>teeth</li>
+                <li>being naked in public</li>
+                <li>pregnancy</li>
+                <li>flying</li>
+                <li>cheating</li>
+                <li>false awakening</li>
+            </ul>
         </div>
     )
 }
