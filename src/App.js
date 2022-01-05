7928119d0661294580f5f8286699e5d2459e0c89
Jamie <drpcom@yahoo.com>
2022-01-04 18:11:38 -0600

    Implemented switch to flex & content component routing.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 import Navbar from './Navbar.js';
 import Sidebar from './Sidebar.js';
-import Home from './Home.js';
+import Content from './Content.js';
 import Browse from './Browse.js';
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="main-container">
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/browse" element={<Browse />} />
-        </Routes>
-        <Sidebar />
+        <Navbar />
+        <div className="main-container">
+        <div className="main-content">
+          <div className="main">
+            <Content />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/browse" element={<Browse />} />
+            </Routes>
+          </div>
+          <Sidebar />
+          </div>
         </div>
-      </div>
+      
     </Router>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-container">
             <h1>this is the home page</h1>
         </div>
     )
