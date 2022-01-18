054c444fae74ce81b66a089c22a22a60666ede6b
Jamie <drpcom@yahoo.com>
2022-01-17 22:05:32 -0600

    Fixed dead link.

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -7,7 +7,7 @@ const Terms = () => {
         <Banner name="Terms of Use" link="/terms" />
         <div className="boh-container">
             <h1 className="boh-title">Terms of Use</h1>
-            <h3 className="boh-content">Articles and content on Dreamy.io (excluding photos, all of which are provided by Unsplash) are the exclusive property of Dreamy.io and are protected by copyright and intellectual property laws, all rights reserved.</h3>
+            <h3 className="boh-content">Articles and content on Dreamy (excluding photos, all of which are provided by <a href="http://unsplash.com">Unsplash</a>) are the exclusive property of Dreamy and are protected by copyright and intellectual property laws, all rights reserved.</h3>
             <h3 className="boh-content">Any reproduction of this site's content should be allowed only with the expressed permission of the <a href="/contact">webmaster</a>.</h3>
         </div>
         </>
