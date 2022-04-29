05d8e19281b5046ed6ba3d28f6bcc43f22d76648
Jamie <drpcom@yahoo.com>
2022-04-29 16:47:29 -0500

    Removed dashes in article search.

--- src/components/articleSearch.js
+++ src/components/articleSearch.js
@@ -1,66 +1,81 @@
-import React,{ useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './SearchBar.css';
+import React, { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./SearchBar.css";
 
-const SearchBar = ({allArticles}) => {
+const SearchBar = ({ allArticles }) => {
+  const [filteredData, setFilteredData] = useState([]);
 
-    const [filteredData, setFilteredData] = useState([]);
+  let useClickOutside = (handler) => {
+    let domNode = useRef();
+    useEffect(() => {
+      let maybeHandler = (e) => {
+        if (domNode.current && !domNode.current.contains(e.target)) {
+          handler();
+        }
+      };
 
-    let useClickOutside = (handler) => {
-        let domNode = useRef()
-        useEffect(() => {
-            let maybeHandler = (e) => {
-            if (domNode.current && !domNode.current.contains(e.target)) {
-                handler();
-            }
-            };
-    
-            document.addEventListener("mousedown", maybeHandler)
-            
-            return () => {
-                document.removeEventListener("mousedown", maybeHandler)
-            }
-        },[handler]);
+      document.addEventListener("mousedown", maybeHandler);
 
-        return domNode;
-    }
+      return () => {
+        document.removeEventListener("mousedown", maybeHandler);
+      };
+    }, [handler]);
 
-    let data = [];
-    allArticles.forEach((article) => {
-        data.push(article.route);
-        // console.log(data)
-    })
+    return domNode;
+  };
 
-    const handleFilter = (e) => {
-        const searchWord = e.target.value;
-        const newFilter = data.filter((value => {
-            return value.toLowerCase().includes(searchWord.toLowerCase());
-        }))
-        searchWord === "" ? setFilteredData([]) : setFilteredData(newFilter)
-    }
+  let data = [];
+  allArticles.forEach((article) => {
+    data.push(article.route);
+    // console.log(data)
+  });
 
-    let domNode = useClickOutside(() => {
-        setFilteredData([]);
-    })
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    const newFilter = data.filter((value) => {
+      return value.toLowerCase().includes(searchWord.toLowerCase());
+    });
+    searchWord === "" ? setFilteredData([]) : setFilteredData(newFilter);
+  };
 
-    const clearResults = () => {
-        setFilteredData([])
-    }
-    return (
-        <>
-            <div className="article-search-input">
-                <input type="text" placeholder='Search' onChange={handleFilter} />
-                <div className="search-icon"></div>
-            </div>
-            {filteredData.length !== 0 && (
-            <div className="article-data-result" ref={domNode} onClick={clearResults}>
-                {filteredData.slice(0, 15).map((value, i) => {
-                    return <Link to={`/articles/${value}`} key={i} className="article-search-link">{value}</Link>
-                })}
-            </div>
-            )}
-        </>
-    )
-}
+  let domNode = useClickOutside(() => {
+    setFilteredData([]);
+  });
 
-export default SearchBar
+  const clearResults = () => {
+    setFilteredData([]);
+  };
+  return (
+    <>
+      <div className="article-search-input">
+        <input
+          type="text"
+          placeholder="Search Articles"
+          onChange={handleFilter}
+        />
+        <div className="search-icon"></div>
+      </div>
+      {filteredData.length !== 0 && (
+        <div
+          className="article-data-result"
+          ref={domNode}
+          onClick={clearResults}
+        >
+          {filteredData.slice(0, 15).map((value, i) => {
+            return (
+              <Link
+                to={`/articles/${value}`}
+                key={i}
+                className="article-search-link"
+              >
+                {value.replace(/-/g, " ")}
+              </Link>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SearchBar;
