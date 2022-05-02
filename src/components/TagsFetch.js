b2983eeba360bbd5ea044782b1cf137b4b422e4c
Jamie <drpcom@yahoo.com>
2022-05-01 19:26:47 -0500

    Added document ID to environment sample.

--- src/components/TagsFetch.js
+++ src/components/TagsFetch.js
@@ -1,30 +1,29 @@
-import {useState, useEffect} from 'react';
-import { db } from '../backend/firebase_config';
-import 'firebase/auth';
-import 'firebase/firestore';
+import { useState, useEffect } from "react";
+import { db } from "../backend/firebase_config";
+import "firebase/auth";
+import "firebase/firestore";
 
 export function useTagsFetch() {
-    const [everyTag, setEveryTag] = useState([]);
+  const [everyTag, setEveryTag] = useState([]);
 
-        useEffect(() => {
-            
-            const retrieveAll = async () => {
-                try {
-                    const tagRef = db.collection("Tags").doc('9mbewr5gGihaX2sefiOG');
-                    const document = await tagRef.get();
+  useEffect(() => {
+    const retrieveAll = async () => {
+      try {
+        const tagRef = db
+          .collection("Tags")
+          .doc(process.env.REACT_APP_DOCUMENT_ID);
+        const document = await tagRef.get();
 
-                    const data = document.data().tags;
-                    setEveryTag(data)
-                    
-                } catch (error) {
-                    console.log("Unable to grab tags :(")
-                }
-            }
-            
-            retrieveAll();
-            
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        },[])
-        return {everyTag, setEveryTag};
+        const data = document.data().tags;
+        setEveryTag(data);
+      } catch (error) {
+        console.log("Unable to grab tags :(");
+      }
+    };
 
-    }
+    retrieveAll();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return { everyTag, setEveryTag };
+}
